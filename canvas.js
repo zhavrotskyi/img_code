a825f39671ddbf5d2b0d6d4d43fe95a82787d471
zhavrotskyi <d.zhavrotskyi@gmail.com>
2024-11-07 19:31:13 +0200

    examples

--- canvas.js
+++ canvas.js
@@ -1,29 +1,70 @@
-const mm = 2;
-const defVal = 1;
+const mm = 5;
+const defVal = 0;
 const direct = true;
+const evenInverted = false;
 
 function copySize(inEl, outEl) {
-  //console.log ("in " +  outEl.width + " * " + outEl.height+ " (" + inEl.width+ " * " + inEl.height + ")");
   outEl.width = inEl.width;
   outEl.height = inEl.height;
 }
 
-// function colorAdd (color1, color2) {
-
-// }
-
 function colorsAdd(toColor, adColor) {
-  console.log(adColor);
+  //console.log(adColor);
   for (let index = 0; index < 3; index++) {
     toColor.data[index] ^= adColor[index];
-    //toColor.data  [index] ^= adColor [index];
-    //toColor.data [index] = toColor.data [index] ^ adColor [index] ;
   }
 }
 
+
+function codeImage (inCanvas, outCanvas, testCanvas) {
+  outCanvas = document.getElementById("output_canvas");
+
+  const m = document.getElementById ("mm").value;
+  console.log (m);
+  const steps = 0;
+
+  flTest = false;
+  if (flTest == 3) {
+    let a = 0;
+    let b = 0;
+    let roots = getRoots(m);
+    console.log(roots);
+    let batch = getBatch(a, b, roots);
+    console.log(batch);
+  } else if (flTest == true) {
+    let qC = new QuasiCrystal(3);
+    console.log(qC.roots);
+    let batch = qC.getBatch(3);
+  } else {
+    moveImgLines(inCanvas, outCanvas, steps, m);
+    moveImgLines(outCanvas, testCanvas, -steps, m);
+  }
+
+}
+
+function toggleButton (mode = true) {
+  let btn = document.getElementById ("selectFile");
+  btn.disabled = mode;
+  btn = document.getElementById ("recode");
+  btn.disabled = mode;
+}
+
+function recode () {
+  toggleButton (true);
+  let inCanvas = document.getElementById("input_canvas");
+  let outCanvas = document.getElementById("output_canvas");
+  let testCanvas = document.getElementById("test_canvas");
+  codeImage (inCanvas, outCanvas, testCanvas);
+  toggleButton (false);
+}
+
 function openFile(file) {
+
   let input = file.target;
   let reader = new FileReader();
+  if (input.files.length > 0) {
+    toggleButton (true);
+  }
 
   reader.onload = function () {
     let dataURL = reader.result;
@@ -48,34 +89,9 @@ function openFile(file) {
       copySize(inImg, testCanvas);
 
       inCanvasContex.drawImage(inImg, 0, 0);
+      codeImage (inCanvas, outCanvas, testCanvas);
+      toggleButton (false);
 
-      //console.log ("after draw " +  inCanvasContex.width + " * " + inCanvasContex.height);
-
-      outCanvas = document.getElementById("output_canvas");
-
-      // moveImgLines(inCanvas, outCanvas);
-      // moveImgLines(outCanvas, testCanvas, -1);
-
-      const m = mm;
-      const steps = 0;
-
-      flTest = false;
-      if (flTest == 3) {
-        let a = 0;
-        let b = 0;
-        let roots = getRoots(m);
-        console.log(roots);
-        let batch = getBatch(a, b, roots);
-        console.log(batch);
-      } else if (flTest == true) {
-        let qC = new 
-        QuasiCrystal (3);
-        console.log(qC.roots);
-        let batch = qC.getBatch (3);
-      }  else {
-        moveImgLines(inCanvas, outCanvas, steps, m);
-        moveImgLines(outCanvas, testCanvas, -steps, m);
-      }
     };
   };
 
@@ -86,54 +102,63 @@ class QuasiCrystal {
   constructor(m = 3) {
     this.a = 0;
     this.b = 0;
-    this.roots = QuasiCrystal.getRoots (m);
+    this.roots = QuasiCrystal.getRoots(m);
   }
 
-  getBit (defVal = 1) {
-    this.b ++;
+  getBit(defVal = 1) {
+    this.b++;
     const candidate = this.a + this.roots[0] * this.b;
-    //console.log(candidate);
-    // console.log("a:");
-    // console.log(this.a);
-    // console.log("b:");
-    // console.log(this.b);
     let res;
     if (candidate >= 0 && candidate < 1) {
-      res = defVal - 0;
+      res = defVal;
     } else {
-      this.a ++;
-      res = defVal - 1;
+      this.a++;
+      res = 1 - defVal;
     }
-    // console.log("a:");
-    // console.log(this.a);
-    // console.log("b:");
-    // console.log(this.b);
-    // console.log("res:");
-    // console.log(res);
     return res;
   }
 
-  getByte (flDirect = true, defVal = 1) {
+  getByte(flDirect = true, defVal = 1, flInv = false) {
     const bitsPerByte = 8;
     let biNum = "";
     //const flInv = false;
-    for (let index = 0; index < bitsPerByte; index++) {
-      if (flInv && index % 2 == 0) {
-        biNum += 1 - this.getBit ();
-      } else {
-        biNum += this.getBit ();
+    if (flDirect == true) {
+      for (let index = 0; index < bitsPerByte; index++) {
+        if (flInv && index % 2 == 0) {
+          biNum += 1 - this.getBit(defVal);
+        } else {
+          biNum += this.getBit(defVal);
+        }
+      }
+    } else {
+      let curQty = 0;
+      let curLength = 0;
+      while (curLength < bitsPerByte) {
+        if (this.getBit() == 1) {
+          const bitValue = curQty % 2 == 0 ? 1 : 0;
+          if (flInv && index % 2 == 0) {
+            biNum += 1 - bitValue;
+          } else {
+            biNum += bitValue;
+          }
+          curLength++;
+          curQty = 0;
+        } else {
+          curQty++;
+        }
       }
     }
-    console.log (biNum);
+    //console.log(biNum);
     return parseInt(biNum, 2);
   }
-  
-  getBatch (flDirect = true, defVal = 1, bytes = 3) {
+
+  getBatch(flDirect = true, defVal = 1, flInv = false, bytes = 3) {
     let batch = [];
+
     for (let index = 0; index < bytes; index++) {
-      batch.push (this.getByte (flDirect, defVal));
+      batch.push(this.getByte(flDirect, defVal, flInv));
     }
-  
+
     return batch;
   }
 
@@ -144,55 +169,9 @@ class QuasiCrystal {
   }
 }
 
-function _getRoots(m = 3) {
-  const D = Math.sqrt(m * m + 4);
-  const roots = [(m - D) / 2, (m + D) / 2];
-  return roots;
-}
-
-function _getBit(a, b, roots) {
-  b = b + 1;
-  const candidate = a + roots[0] * b;
-  console.log(candidate);
-  console.log("a:");
-  console.log(a);
-  console.log("b:");
-  console.log(b);
-  let res;
-  if (candidate >= 0 && candidate < 1) {
-    res = 0;
-  } else {
-    a = a + 1;
-    res = 1;
-  }
-  console.log("a:");
-  console.log(a);
-  console.log("b:");
-  console.log(b);
-  console.log("res:");
-  console.log(res);
-}
-
-function _getByte(a, b, roots) {
-  const bitsPerByte = 8;
-  let biNum = "";
-  for (let index = 0; index < bitsPerByte; index++) {
-    biNum += getBit(a, b, roots);
-  }
-  return parseInt(biNum, 2);
-}
-
-function _getBatch(a, b, roots, bytes = 3) {
-  let batch = [];
-  for (let index = 0; index < bytes; index++) {
-    batch.push(getByte(a, b, roots));
-  }
-  //console.log (batch);
-
-  return batch;
-}
-
 function moveImgLines(inCanvas, outCanvas, step = 1, m = 0) {
+  const showProgress = false;
+  const elProgress = document.getElementById("myBar");
   const imHeight = inCanvas.height;
   const imWidth = outCanvas.width;
   inCContext = inCanvas.getContext("2d");
@@ -202,27 +181,25 @@ function moveImgLines(inCanvas, outCanvas, step = 1, m = 0) {
   let addForNegative = 0;
   if (imWidth > 0 && step < 0) {
     addForNegative = Math.ceil(imHeight / imWidth + 2) * imWidth;
-    console.log(addForNegative);
+    //console.log(addForNegative);
   }
 
   let colorAdd;
 
-  // let roots = getRoots(m);
-  // let a = 0;
-  // let b = 0;
   let myQC;
 
   if (m == 0) {
     colorAdd = [100, 150, 200];
   } else {
-    myQC = new QuasiCrystal (m)
+    myQC = new QuasiCrystal(m);
   }
 
   const flTest = false;
-
-  //console.log (getRoots ());
-
-  // let logsCount = 0;
+  let prevTime = new Date();
+  let totalProgress = imHeight * imWidth;
+  let currentProgress = 0;
+  let currentProgressShown = 0;
+  elProgress.style.width = currentProgressShown + "%";
 
   for (let y = 0; y < imHeight; y++) {
     for (let x = 0; x < imWidth; x++) {
@@ -232,18 +209,10 @@ function moveImgLines(inCanvas, outCanvas, step = 1, m = 0) {
         console.log(x + " / " + y);
         console.log(pixelColor);
       }
+
       if (m != 0) {
-        // if (logsCount < 23) {
-        //   logsCount++;
-        //   console.log("ribbit");
-        // }
-        //colorAdd = getBatch(a, b, roots);
-        colorAdd = myQC.getBatch (direct, defVal);
+        colorAdd = myQC.getBatch(direct, defVal, evenInverted);
       }
-      // if (logsCount < 23) {
-      //   logsCount++;
-      //   console.log(colorAdd);
-      // }
 
       colorsAdd(pixelColor, colorAdd);
 
@@ -253,11 +222,35 @@ function moveImgLines(inCanvas, outCanvas, step = 1, m = 0) {
 
       xNew = (x + addForNegative + y * step) % imWidth;
       outCContext.putImageData(pixelColor, xNew, y);
+      if (showProgress) {
+        currentProgress = Math.trunc(((y * imWidth + x) / totalProgress) * 100);
+
+        if (currentProgress - currentProgressShown > 7) {
+          let curTime = new Date();
+          // console.log ("@@@@@@@@@@");
+          // console.log(curTime - prevTime);
+
+          if (curTime - prevTime > 150) {
+            prevTime = curTime;
+            console.log(currentProgress);
+            console.log(currentProgress);
+            currentProgressShown = currentProgress;
+            elProgress.style.width = currentProgressShown + "%";
+            console.log(elProgress.style.width);
+            setTimeout(1000);
+          }
+        }
+      }
+
       if (flTest) {
         if (x > 2 || y > 2) {
           return;
         }
       }
     }
+  }
+  if (showProgress) {
+    currentProgressShown = 100;
+    elProgress.style.width = currentProgressShown + "%";
   }
 }
